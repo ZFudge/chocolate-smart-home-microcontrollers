9a3b2a71e7119175bda38247a29c46ddc857ad10
ZFudge <zacheryfudge+github@gmail.com>
2024-03-25 13:18:13 -0600

    remove explicit use of mqtt port and server values after mqtt initialization

--- on_off_controller/on_off_controller.ino
+++ on_off_controller/on_off_controller.ino
@@ -2,24 +2,20 @@
 #include "consts.h"
 #include "data_funcs.h"
 
-
 void setup() {
   Serial.begin(115200);
-  Serial.print("\nInit ");
-  Serial.print(CONTROLLER_TYPE);
-  Serial.println(" ESP /w CTRLR ID: ");
-  Serial.print(MQTT_ID);
-  Serial.println(" & CTRLR NAME: " + controller.name);
 
-  randomSeed(analogRead(0));
-  pinMode(LED_BUILTIN, OUTPUT);
   controller.init(CONTROLLER_TYPE, MQTT_ID, NAME, getStateMessage);
+
   setup_wifi();
+
   init_mqtt_broker(handleMqttMsgReceivedOnOff, MQTT_SERVER, MQTT_PORT);
+
+  pinMode(LED_BUILTIN, OUTPUT);
   // pinMode(ON_OR_OFF_PIN, OUTPUT);
 }
 
 void loop() {
-  if (!mqtt_client.connected()) connect_mqtt_broker(MQTT_PORT, MQTT_SERVER);
+  if (!mqtt_client.connected()) connect_mqtt_broker();
   mqtt_client.loop();
 }
